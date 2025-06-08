d4b5466e37371380dad63ec9f3a06974faab57bf
JAYDEN <aphorism037@naver.com>
2025-06-09 03:34:09 +0900

    불필요 파일 삭제

--- SQL/INSERT문/batter.sql
+++
@@ -1,6 +0,0 @@
--- batter 테이블 데이터 추가
-INSERT INTO batter (batterPosition_id, batterStats_id)
-SELECT bp.id, bs.id
-FROM batterPosition bp
-JOIN positioned d ON bp.positioned_id = d.id
-JOIN batterStats bs ON d.players_id = bs.players_id;
--- SQL/INSERT문/pitcher.sql
+++
@@ -1,6 +0,0 @@
--- pitcher 테이블 데이터 추가
-INSERT INTO pitcher (pitcherPosition_id, pitcherStats_id)
-SELECT pp.id, ps.id
-FROM pitcherPosition pp
-JOIN positioned d ON pp.positioned_id = d.id
-JOIN pitcherStats ps ON d.players_id = ps.players_id;
